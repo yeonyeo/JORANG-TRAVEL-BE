db74d4781fa965a762af28ae872e5880a4435707
Kim Sehyun <kkshyun56@gmail.com>
2024-06-10 11:07:41 +0900

    [fix] AuthExceptionController

--- src/main/java/com/example/travel_diary/controller/exceptionController/AuthExceptionController.java
+++ src/main/java/com/example/travel_diary/controller/exceptionController/AuthExceptionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class authExceptionController {
+public class AuthExceptionController {
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
